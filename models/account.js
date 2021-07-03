7a92a7faa3d476a4e3221b7d187f25bc90ad083b
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-07-03 20:16:51 -0300

    alteração no model

---
+++ models/account.js
@@ -0,0 +1,38 @@
+const find = (prisma) => async (data) => {
+  let result = await prisma.account.findUnique({
+    where: { number: data },
+    select: {
+      number: true,
+      balance_available: true,
+    },
+  });
+  return result;
+};
+
+const updateBalance = (prisma) => async (total) => {
+  let result = await prisma.account.update({
+    where: { number: account },
+    select: {
+      number: true,
+      balance_available: true,
+    },
+    data: {
+      balance_available: total,
+    },
+  });
+};
+
+const withdraw = (prisma) => async (account, value) => {
+  const total = account.balance_available - value;
+  return await updateBalance(prisma)(account.number, total);
+};
+const deposit = (prisma) => async (account, value) => {
+  const total = account.balance_available + value;
+  return await updateBalance(prisma)(account.number, total);
+};
+
+module.exports = {
+  withdraw,
+  deposit,
+  find,
+};
